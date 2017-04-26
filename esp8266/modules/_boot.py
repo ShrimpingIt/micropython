885a2e10816177aaf39207b8a05279c6a7708c44
Cefn Hoile <github.com@cefn.com>
2017-04-26 06:25:10 +0100

    Do not launch VFAT filesystem agent as unused if relying on frozen modules.

--- esp8266/modules/_boot.py
+++ esp8266/modules/_boot.py
@@ -1,6 +1,7 @@
 import gc
 gc.threshold((gc.mem_free() + gc.mem_alloc()) // 4)
 import uos
+"""
 from flashbdev import bdev
 
 try:
@@ -11,5 +12,5 @@ try:
 except OSError:
     import inisetup
     vfs = inisetup.setup()
-
+"""
 gc.collect()
